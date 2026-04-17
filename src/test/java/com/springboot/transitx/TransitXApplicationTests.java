6f0b3128c6539476386b4ea35da0a3c820e51afa
mansi <mansiakkr@gmail.com>
2026-04-17 21:30:58 +0530

    test: add integration and unit tests for authentication

---
+++ src/test/java/com/springboot/transitx/TransitXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.transitx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
