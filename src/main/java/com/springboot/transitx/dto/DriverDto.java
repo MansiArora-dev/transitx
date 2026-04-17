62fc2e87d0af58b729769dd5ebbe62e6d9cb901b
mansi <mansiakkr@gmail.com>
2026-04-17 20:57:43 +0530

    feat: implement driver onboarding and management

---
+++ src/main/java/com/springboot/transitx/dto/DriverDto.java
@@ -0,0 +1,18 @@
+package com.springboot.transitx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverDto {
+
+    private Long id;
+    private UserDto user;
+    private Double rating;
+
+    private Boolean available;
+    private String vehicleId;
+}
---
+++ src/main/java/com/springboot/transitx/services/DriverService.java
@@ -0,0 +1,33 @@
+package com.springboot.transitx.services;
+
+import com.springboot.transitx.dto.DriverDto;
+import com.springboot.transitx.dto.RideDto;
+import com.springboot.transitx.dto.RiderDto;
+import com.springboot.transitx.entities.Driver;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+public interface DriverService {
+
+    RideDto acceptRide(Long rideRequestId);
+
+    RideDto cancelRide(Long rideId);
+
+    RideDto startRide(Long rideId, String otp);
+
+    RideDto endRide(Long rideId);
+
+    RiderDto rateRider(Long rideId, Integer rating);
+
+    DriverDto getMyProfile();
+
+    Page<RideDto> getAllMyRides(PageRequest pageRequest);
+
+    Driver getCurrentDriver();
+
+    Driver updateDriverAvailability(Driver driver, boolean available);
+
+    Driver createNewDriver(Driver driver);
+}
