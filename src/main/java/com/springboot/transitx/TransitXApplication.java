779643e1279330c7e6cdecce342d405198d73031
mansi <mansiakkr@gmail.com>
2026-04-17 19:35:49 +0530

    chore: initialize Spring Boot project with Maven wrapper and dependencies

---
+++ src/main/java/com/springboot/transitx/TransitXApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.transitx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitXApplication.class, args);
+	}
+
+}
